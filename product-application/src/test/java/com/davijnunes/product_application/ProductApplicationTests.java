7403e237f557e3abec4205c5928f875b24dec49f
likearockstars <likerockstar254@gmail.com>
2025-06-05 17:38:48 -0300

    Criando a estrutura do projeto Spring

---
+++ product-application/src/test/java/com/davijnunes/product_application/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davijnunes.product_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
